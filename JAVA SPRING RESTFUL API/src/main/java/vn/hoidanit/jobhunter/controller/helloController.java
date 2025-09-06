a76453ccf51162e7d332c3b8299855c002b029eb
Tín <Trungtin0701200@gmail.com>
2025-09-06 18:45:06 +0700

    Up lên chơi thôi chứ chưa chạy dc

--- JAVA SPRING RESTFUL API/src/main/java/vn/hoidanit/jobhunter/controller/helloController.java
+++ JAVA SPRING RESTFUL API/src/main/java/vn/hoidanit/jobhunter/controller/helloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping("/")
     public String index() {
         return "Hello World from Spring Boot with @hoidanit";
--- JAVA SPRING RESTFUL API/src/main/java/vn/hoidanit/jobhunter/jobhunterApplication.java
+++ JAVA SPRING RESTFUL API/src/main/java/vn/hoidanit/jobhunter/jobhunterApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(exclude =
 org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
-public class jobhunterApplication {
 
+public class JobhunterApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(jobhunterApplication.class, args);
+		SpringApplication.run(JobhunterApplication.class, args);
 	}
 
 }
